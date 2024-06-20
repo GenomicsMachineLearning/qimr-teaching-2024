ba7bb8f184d18faca46795e811181b68c0635fcf
Andrew Newman <andrewfnewman@gmail.com>
2024-06-20 10:03:37 +1000

    Add.

---
+++ runme.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+node=$(hostname -I | cut -f1 -d' ')
+user=$(whoami)
+
+for port in {3500..3700}; do
+        if ! ss -lptn | grep -q ":$port"; then
+                echo "Port $port is available"
+                break
+        fi
+done
+
+echo -e "
+Command to create ssh tunnel:
+ssh -N -L ${port}:${node}:${port} ${user}@${node}
+Use a Browser on your local machine to go to:
+localhost:${port}  (prefix w/ https:// if using password)
+"
+
+cd /software/qimr-teaching-2024
+jupyter notebook --no-browser --port=${port} --ip=0.0.0.0
