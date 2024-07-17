8423a74b13a140d6c5dbb524b74e5fb4cb51c742
Andrew Newman <andrewfnewman@gmail.com>
2024-07-17 11:08:43 +1000

    Update with versions.

--- dependencies.R
+++ dependencies.R
@@ -3,11 +3,17 @@ options(repos = c(CRAN = "https://mirror.aarnet.edu.au/pub/CRAN/"))
 
 install.packages("remotes", dependencies = FALSE)
 remotes::install_version('dlm', dependencies = FALSE)
+# Install version 1.15.2
 remotes::install_github("drighelli/SpatialExperiment", dependencies=FALSE)
+# Install version 1.7.0
 remotes::install_github("pachterlab/SpatialFeatureExperiment", ref="devel", dependencies=FALSE)
+# Install version 1.7.0
 remotes::install_github("pachterlab/Voyager", ref="devel", dependencies=FALSE)
 remotes::install_version('wrMisc', dependencies = FALSE)
+# Install version 1.1
 remotes::install_github('YingMa0107/CARD', dependencies = FALSE)
 remotes::install_github('renozao/NMF', ref="0.30.4.900", dependencies = FALSE)
+# Install version 2.1.2
 remotes::install_github('jinworks/CellChat', dependencies = FALSE)
+# Install version 1.1.0
 remotes::install_github('navinlabcode/copykat', dependencies = FALSE)
